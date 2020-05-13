72191d1c6a0cd030d50ce27b484986ece001d238
dohyunmoo <55354386+dohyunmoo@users.noreply.github.com>
2020-05-13 15:34:39 -0400

    Update Pong.py

--- Pong.py
+++ Pong.py
@@ -1,6 +1,7 @@
 import pygame
 pygame.init()
 from numpy import random
+import Class as C
 
 win = pygame.display.set_mode((500, 500))
 
@@ -8,33 +9,14 @@ pygame.display.set_caption("Pong")
 
 screenwidth = 500
 
-width = 25
-height = 100
-
-x1 = (screenwidth - width)
-y1 = 200
-
-x2 = 0
-y2 = 200
-
-vel = 5
-
-ballr = 10 #radius
-balld = ballr + ballr #diameter
-
-ballv_x = 4 #hor. vel
-ballv_y = 3 #ver. vel
-
-ballx = 250
-bally = 100
-ballInitiate = False
-ballHor = True
-ballVer = True
-
 score1 = 0
 score2 = 0
 
 run = True
+
+ball = C.Ball()
+player1 = C.Player(screenwidth - 25)
+player2 = C.Player(0)
 
 while run:
     pygame.time.delay(10)
@@ -47,84 +29,88 @@ while run:
     font = pygame.font.Font(None, 36)
 
     # player 1 (Right)
-    if keys[pygame.K_UP] and y1 > 0:
-        y1 -= vel
-    if keys[pygame.K_DOWN] and y1 < (screenwidth - height):
-        y1 += vel
+    if keys[pygame.K_UP] and player1.y > 0:
+        player1.y -= player1.vel
+    if keys[pygame.K_DOWN] and player1.y < (screenwidth - player1.height):
+        player1.y += player1.vel
 
     # player 2 (Left)
-    if keys[pygame.K_w] and y2 > 0:
-        y2 -= vel
-    if keys[pygame.K_s] and y2 < (screenwidth - height):
-        y2 += vel
+    if keys[pygame.K_w] and player2.y > 0:
+        player2.y -= player2.vel
+    if keys[pygame.K_s] and player2.y < (screenwidth - player2.height):
+        player2.y += player2.vel
 
     # ball
-    if ballInitiate:
-        if ballHor:
-            ballx -= ballv_x
+    if ball.ballInitiate:
+        if ball.ballHor:
+            ball.ballx -= ball.ballv_x
         else:
-            ballx += ballv_x
+            ball.ballx += ball.ballv_x
 
-        if ballVer:
-            bally -= ballv_y
-            if bally <= ballr:
-                ballVer = False
+        if ball.ballVer:
+            ball.bally -= ball.ballv_y
+            if ball.bally <= ball.ballr:
+                ball.ballVer = False
         else:
-            bally += ballv_y
-            if bally >= (screenwidth - ballr):
-                ballVer = True
+            ball.bally += ball.ballv_y
+            if ball.bally >= (screenwidth - ball.ballr):
+                ball.ballVer = True
 
     # Collision
-    if (ballx + ballr) >= x1 and (y1 <= bally <= (y1 + height)):
-        ballHor = True
+    if (ball.ballx + ball.ballr) >= player1.x and (player1.y <= ball.bally <= (player1.y + player1.height)):
+        ball.ballHor = True
 
-    if (ballx - ballr) <= (x2 + width) and (y2 <= bally <= (y2 + height)):
-        ballHor = False
+    if (ball.ballx - ball.ballr) <= (player2.x + player2.width) and (player2.y <= ball.bally <= (player2.y + player2.height)):
+        ball.ballHor = False
 
     # Game Over
-    if (ballx + ballr) >= screenwidth:
+    if (ball.ballx + ball.ballr) >= screenwidth:
         # print("Player 2 wins a point")
         score2 += 1
-        ballx = 250;bally = 100;ballInitiate = False
+        ball.ballx = 250
+        ball.bally = 100
+        ball.ballInitiate = False
 
-    if (ballx - ballr - ballv_x) <= 0:
+    if (ball.ballx - ball.ballr - ball.ballv_x) <= 0:
         # print("Player 1 wins a point")
         score1 += 1
-        ballx = 250;bally = 100;ballInitiate = False
+        ball.ballx = 250
+        ball.bally = 100
+        ball.ballInitiate = False
 
-    win.fill((0, 0, 0))
+    win.fill((255, 255, 255))
 
-    text = font.render(str(score2), 1, (255, 255, 255))
+    text = font.render(str(score2), 1, (0, 0, 0))
     win.blit(text, (150, 10))
-    text = font.render(str(score1), 1, (255, 255, 255))
+    text = font.render(str(score1), 1, (0, 0, 0))
     win.blit(text, (350, 10))
 
-    if not ballInitiate:
+    if not ball.ballInitiate:
         font = pygame.font.Font(None, 18)
         direction = "Press SPACE key to Begin..."
-        text = font.render(direction, 1, (255, 255, 255))
+        text = font.render(direction, 1, (0, 0, 0))
         win.blit(text, (180, 50))
 
         if keys[pygame.K_SPACE]:
             x = random.randint(4)
             if x == 0:
-                ballHor = False
-                ballVer = False
+                ball.ballHor = False
+                ball.ballVer = False
             elif x == 1:
-                ballHor = True
-                ballVer = False
+                ball.ballHor = True
+                ball.ballVer = False
             elif x == 2:
-                ballHor = False
-                ballVer = True
+                ball.ballHor = False
+                ball.ballVer = True
             else:
-                ballHor = True
-                ballVer = True
+                ball.ballHor = True
+                ball.ballVer = True
 
-            ballInitiate = True
+            ball.ballInitiate = True
 
-    pygame.draw.rect(win, (255, 0, 0), (x1, y1, width, height))
-    pygame.draw.rect(win, (0, 255, 0), (x2, y2, width, height))
-    pygame.draw.circle(win, (255, 255, 255), (ballx, bally), ballr)
+    pygame.draw.rect(win, (255, 0, 0), (player1.x, player1.y, player1.width, player1.height))
+    pygame.draw.rect(win, (0, 255, 0), (player2.x, player2.y, player2.width, player2.height))
+    pygame.draw.circle(win, (0, 0, 0), (ball.ballx, ball.bally), ball.ballr)
     pygame.display.update()
 
 pygame.quit()
